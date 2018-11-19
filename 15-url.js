c7098fef0648ba6ef1c9c89d603d3700126f6a8e
水瓶座的木木欣 <634144330@qq.com>
2018-11-20 00:44:12 +0800

    路由router

---
+++ 15-url.js
@@ -0,0 +1,17 @@
+var http=require('http');
+var url = require('url');
+//使用依赖注入的方式添加路由模块
+function start(route){
+    function onRequest(request,response){
+        var  pathname=url.parse(request.url).pathname;
+        console.log("Request for"+pathname+"received.");
+        route(pathname);
+        response.writeHead(200,{'Content-Type':"text/plain"});
+        response.write("hello world");
+        response.end();
+    }
+    http.createServer(onRequest).listen(8888);
+    console.log("server has started..");
+    
+}
+exports.start = start;
---
+++ 17-index.js
@@ -0,0 +1,3 @@
+var url = require('./15-url');
+var router = require('./16-router');
+url.start(router.route);
